a417a13097f11cbaf6a75da6b60fc91859843009
Gauge Pendergrass <gauapend@ut.utm.edu>
2017-02-01 14:40:24 -0600

    Stuff for Thursday
    Done ? least title

--- main.php
+++ main.php
@@ -1,22 +1,248 @@
 <!DOCTYPE html>
 <html>
 
+<?php
+  $title = "Charts are Love, Charts are Life";
+  if ($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    $title = $_POST["title"];
+  }
+?>
+
+<head>
+
+  <title><?php echo $title; ?></title>
+
+</head>
+
 <body>
 
-<p> Please enter your first and last name!! </p>
+<h1>
+    Charts are Love, Charts are Life
+</h1>
 
-<!-------------Form, check w3, to ask for input, can include an action parameter, but none right now. using get method means shows in URL --------->
-  <form method = "get">
-    First name: <br>
-      <input type = "text" name = "firstname">
-      <br>
-    Last name: <br>
-      <input type = "text" name = "lastname">
-      <br>
-      <br>
-      <input type = "submit" value = "Submit">
-  </form>
+<form method="post">
 
+  Name / Title: <input type="text" name="title" value="<?php if ($title != "Charts are Love, Charts are Life") { echo $title; } ?>"> <br>
+
+  Chart Type:
+  <select>
+    <option value = "Stats"> Stats </option>
+    <option value = "Bar"> Bar </option>
+    <option value = "Line"> Line </option>
+    <option value = "Pie"> Pie </option>
+  </select>
+  <br>
+
+  Sort By:
+  <select>
+    <option value = "None"> None </option>
+    <option value = "Score"> Score </option>
+    <option value = "Name"> Name </option>
+    <option value = "Last Name"> Last Name </option>
+  </select>
+  <br>
+
+  Data:
+  <textarea rows="5" cols="50" placeholder="Please enter the names and grades seperated by a comma..."></textarea>
+
+  <br>
+
+  <input type = "submit">
+
+</form>
+
+<div style ="background-color: black; width=10;">
+  ayyyyyy
+</div>
+
+
+
+<h1>
+  <?php echo $title ?>
+</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-------------------------------Do not Touch --------------------->
   <?php include 'tables.php';?>
   <?php makeTable(); ?>
 </body>
