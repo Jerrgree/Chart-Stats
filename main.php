1093c6ab1bee1bb8bef50525986bf25e3a9df131
Gauge Pendergrass <gauapend@ut.utm.edu>
2017-01-31 18:23:46 -0600

    syntax
    added </p>

--- main.php
+++ main.php
@@ -1,15 +1,9 @@
 <!DOCTYPE html>
 <html>
 
-
-
-
-
-
-
 <body>
 
-<p> Please enter your first and last name!!
+<p> Please enter your first and last name!! </p>
 
 <!-------------Form, check w3, to ask for input, can include an action parameter, but none right now. using get method means shows in URL --------->
   <form method = "get">
